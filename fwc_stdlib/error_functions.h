c626449bcf1cdf08081e5e58731238bd82fcfcad
Shiva <shvelmur@gmail.com>
2011-10-29 23:05:22 -0700

    standard library

--- fwc_stdlib/error_functions.h
+++ fwc_stdlib/error_functions.h
@@ -6,8 +6,10 @@ void errMsg(const char *format, ...);
 
 #ifdef __GNUC__
 /* This macro stops 'gcc -Wall' complaining that "control reaches
-end of non-void function" if we use the following functions to terminate main() or some other non-void function. */
-#define NORETURN __attribute__ ((__noreturn__)) #else
+   end of non-void function" if we use the following functions to 
+   terminate main() or some other non-void function. */
+#define NORETURN __attribute__ ((__noreturn__)) 
+#else
 #define NORETURN
 #endif
 
--- fwc_stdlib/funwc_hdr.h
+++ fwc_stdlib/funwc_hdr.h
@@ -9,6 +9,11 @@
 #include <errno.h>          /* Declares errno and defines error constants */ 
 #include <string.h>         /* Commonly used string-handling functions */
 
+#include "get_num.h"        /* Declares our functions for handling numeric 
+                               arguments (getInt(), getLong()) */
+
+#include "error_functions.h" /* Declares our error-handling functions */
+
 typedef enum { FALSE, TRUE } Boolean;
 
 #define min(m,n) ((m) < (n) ? (m) : (n)) 
