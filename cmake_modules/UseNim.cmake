ec55ce5fe28d69340e8e1fc87c560d547328c73f
Shiva <shiv@shiv.me>
2014-10-21 05:40:51 -0700

    Nim example with cmake

---
+++ cmake_modules/UseNim.cmake
@@ -0,0 +1,25 @@
+include(CMakeParseArguments)
+
+function(add_nim_executable )
+    cmake_parse_arguments(ARGS "" "TARGET" "SOURCES" ${ARGN})
+
+    set(in_files "")
+    foreach(src ${ARGS_SOURCES} ${ARGS_UNPARSED_ARGUMENTS})
+        list(APPEND in_files "${CMAKE_CURRENT_SOURCE_DIR}/${src}")
+    endforeach(src ${ARGS_SOURCES} ${ARGS_UNPARSED_ARGUMENTS})
+
+    
+
+    set(nim_target "${CMAKE_CURRENT_BINARY_DIR}/${ARGS_TARGET}")
+    set(DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/nimcache")
+
+    add_custom_target(
+    	nim ALL
+    	COMMAND 
+        	${NIM_EXECUTABLE} "c" "--nimcache:" ${DIRECTORY} "--out:" ${nim_target} ${in_files}
+
+    	DEPENDS 
+        	${in_files}
+    )
+
+endfunction(add_nim_executable)
