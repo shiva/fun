6f4c5699eb8e88d8eae03844f4f2427dfa627769
Shiva <shiv@shiv.me>
2015-03-05 23:52:55 -0800

    Markdown to html: command-line utility that uses to-markdown to convert html to markdown

---
+++ node/markdown-to-html/README.md
@@ -0,0 +1,27 @@
+# markdown-to-html [![Build Status](https://secure.travis-ci.org/shiva/fun.png?branch=master)](http://travis-ci.org/shiva/fun)
+
+command-line program to convert markdown to html using to-markdown
+
+## Getting Started
+Install the module with: `npm install markdown-to-html`
+
+```javascript
+var markdown_to_html = require('markdown-to-html');
+markdown_to_html.awesome(); // "awesome"
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2015 Shiva  
+Licensed under the MIT license.
---
+++ node/markdown-to-html/mk-to-html.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+var pkg = require('./package.json');
+var toMarkdown = require('to-markdown').toMarkdown;
+var fs = require('fs');
+var path = require('path');
+
+program
+    .version(pkg.version)
+    .option('-i, --input <html-file>', 'html file to convert');
+
+program.parse(process.argv);
+
+if (!program.input) {
+    throw new Error("An input html file is required.");
+}
+
+console.log('processing ' + program.input + ' ...');
+
+markdownTxt = toMarkdown(fs.readFileSync(program.input, 'utf8'));
+var ifile = path.normalize(program.input);
+
+/* calculate output file name : filename.md */
+var outfile = path.basename(ifile, '.html') + '.md';
+var dirname = path.dirname(ifile);
+
+/* if input file is prefixed with directories
+ * add the directory to output file too 
+ */
+if (dirname != '.') {
+    outfile = dirname + outfile;
+} 
+
+/* write output file */
+fs.writeFile(outfile, markdownTxt, function (err) {
+    if (err) throw err;
+    console.log('converted ' + program.input + ' to ' + outfile + '!');
+});
+
