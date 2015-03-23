c72bd087a0f55ed8f9505e98b3c7deece01d9afd
Shiva <shiv@shiv.me>
2015-03-22 23:07:57 -0700

    Fix includes in Object.h

--- c/ooc/include/Object.h
+++ c/ooc/include/Object.h
@@ -1,5 +1,8 @@
+#include <stdio.h>
+
 #ifndef __OBJECT_H__
 #define __OBJECT_H__
+
 
 extern const void* Object;      /* new(Object); */
 extern const void* Class;       /* new(Class, "name", super, size, sel, meth, ... 0) */
@@ -10,4 +13,4 @@ int 	differ(const void* self, const void* a);
 int 	puto(const void* self, FILE* fp);
 size_t 	sizeOf(const void* self);
 
-#endif // __OBJECT_H__
+#endif /* __OBJECT_H__ */
